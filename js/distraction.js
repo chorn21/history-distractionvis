ba4724569a8f2f446aff46a0aeda35dd44e511e8
Caroline Horn <chorn21@gmail.com>
2013-03-27 22:29:27 -0500

    more stuff

--- js/distraction.js
+++ js/distraction.js
@@ -16,14 +16,17 @@ $(function() {
 	var historyPeriod = processTime(history[history.length-1]) - processTime(history[0]);
 	var songLength = 407; // length of song in seconds
 
-	var blockCount = 20; // can be adjusted
+	var blockCount = 40; // can be adjusted
 	var blockAverages = [];
 	var blockLength = historyPeriod / blockCount;
 	var blockEnd = start + blockLength;
 
+	var intervals = [];
 	var index = 0;
 	var items = 0;
 	var distractionCount = 0;
+
+	var scale = d3.scale.linear().domain([start.getTime(), end.getTime()]).range([0, songLength]);
 
 	function calculateAverages() {
 		for(var i=0; i<history.length; i++) {
@@ -35,6 +38,7 @@ $(function() {
 			}
 			else {
 				blockAverages[index] = distractionCount/items;
+				intervals[index] = processTime(history[i]);
 				index++;
 				items = 1;
 				distractionCount = history[i].distraction ? 1 : 0;
@@ -52,7 +56,20 @@ $(function() {
 		return new Date(year, month-1, date, hour, minute, second);
 	}
 
+	function playAudio() {
+		var waitTime = scale(intervals[0]);
+		$("#concerto").play();
+		for(var i=0; i<blockAverages.length; i++) {
+			setTimeout(function() {
+				var num = Math.floor(blockAverages[i]/10)*10;
+				$("#"+num).play();
+			}, waitTime);
+		}
+	}
 
-
+	$("#buttan").click(function() {
+		calculateAverages();
+		playAudio();
+	});
 
 });
